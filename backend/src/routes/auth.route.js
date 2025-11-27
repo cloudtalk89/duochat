7ec67a2a776b5d885f5c164f447f1f2a49bd15d2
cloudtalk89 <cloudtalk89@gmail.com>
2025-11-27 09:38:41 +0630

    adding slash to route path

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,15 +2,15 @@ import express from "express"
 
 const router = express.Router()
 
-router.get("signup",(req,res)=>{
+router.get("/signup",(req,res)=>{
     res.send("singup route")
 })
 
-router.get("login",(req,res)=>{
+router.get("/login",(req,res)=>{
     res.send("login route")
 })
 
-router.get("logout",(req,res)=>{
+router.get("/logout",(req,res)=>{
     res.send("logout route")
 })
 
